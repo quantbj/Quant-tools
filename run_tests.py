848342c96c542e5e02352826accff9da44644c06
Bjn <bjoern.naundorf@gmail.com>
2017-04-12 13:36:53 +0000

    Changed run_tests to exit with status if tests fail

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,11 @@
 import unittest
+import sys
 
 modules = [ 'rm3dmanipulator' ]
 
 if __name__ == "__main__":
     for mod in modules:
         suite = unittest.TestLoader().discover(mod)
-        unittest.TextTestRunner(verbosity=2).run(suite)
+        result = unittest.TextTestRunner(verbosity=2).run(suite)
+        if not result.wasSuccessful():
+            sys.exit(not result.wasSuccessful())
