31a276db4adaed5dd1ad442e1aec52571cdd1af3
Bjn <bjoern.naundorf@gmail.com>
2017-04-12 14:15:28 +0000

    Implemented add_tag in trade.py

--- rm3dmanipulator/trade.py
+++ rm3dmanipulator/trade.py
@@ -7,6 +7,7 @@ class GenericTrade:
         self.trade_type = self.trade_data[0]
         self.ignore_fx_index = field_index[self.trade_type]['ignore_fx_index']
         self.discount_curve_index = field_index[self.trade_type]['discount_curve_index']
+        self.tags_index = field_index[self.trade_type]['tags_index']
 
     def set_ignore_fx(self, flag):
         self.trade_data[self.ignore_fx_index] = flag
@@ -19,6 +20,10 @@ class GenericTrade:
                 self.trade_data[self.discount_curve_index] = '|'.join(cf_list)
             else:
                 self.trade_data[self.discount_curve_index] = curve
+    
+    def add_tag(self, key, value):
+        self.trade_data[self.tags_index] = self.trade_data[self.tags_index] \
+                    + "|" + key + "|" + value
     
     def get_serialized(self):
         return self.trade_data
