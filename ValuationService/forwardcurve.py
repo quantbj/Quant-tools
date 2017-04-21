4d8df818ecb8412cd3863506acc83850fc15fb27
Bjn <bjoern.naundorf@gmail.com>
2017-04-21 15:09:04 +0000

    Added simple-value-objects to travis-ci.yml

--- ValuationService/forwardcurve.py
+++ ValuationService/forwardcurve.py
@@ -1,21 +1,11 @@
 
-# TODO: Convert to generic value objects
+from simple_value_object import ValueObject
 
-
-class IRForwardCurve():
+class IRForwardCurve(ValueObject):
     def __init__(self, ccy, name):
-        self.ccy = ccy
-        self.name = name
-
-    def __eq__(self, other):
-        return ((self.ccy == other.ccy) and (self.name == other.name))
-
+        pass
 
 class FxForwardCurve():
     def __init__(self, ccy_domestic, ccy_foreign):
-        self.ccy_domestic = ccy_domestic
-        self.ccy_foreign = ccy_foreign
-
-    def __eq__(self, other):
-        return ((self.ccy_domestic == other.ccy_domestic)
-                and (self.ccy_foreign == other.ccy_foreign))
+        pass
+    
